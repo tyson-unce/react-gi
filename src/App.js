8c0cedbb07d8540187d541d09550aaea08c7a311
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-07-19 20:32:25 -0500

    initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      name: tyson
+      number: xxx-xxx
+      DOB: 09/25/2001
+      
+    </div>
+  );
+}
+
+export default App;
