b746ed4fca0626c238e6409b82f222736cd0d7ea
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-07-19 20:36:49 -0500

    very easy / easy / medium challenges done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';  
+import BasicInfo from './components/BasicInfo';
 
-function App() {
-  return (
-    <div className="App">
-      name: tyson
-      number: xxx-xxx
-      DOB: 09/25/2001
-      
-    </div>
-  );
+
+let people = [
+  {
+    name: 'Tyson',
+    phone: 'XXX-XXXX',
+    age: 300,
+    favoriteFood: 'Pizza'
+  },
+  {
+    name: 'Sally',
+    phone: 'XXX-XXXX',
+    age: 18,
+    favoriteFood: 'Steak'
+  },
+
+]
+
+function iteratePeople(arr) {
+  return arr.map((person) => {
+    return <BasicInfo person={person} />
+  }
+  )
 }
 
-export default App;
+export default class App extends Component {  
+  // easy
+  constructor(props){  
+    super(props);  
+    this.state = {
+        person: {
+        }
+    }
+  }  
+  // very easy/ medium
+    render() {
+      return (iteratePeople(people))
+  }
+}
